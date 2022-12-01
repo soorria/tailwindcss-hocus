80ceba4cb50e018392b4af2544089ea3cc72c138
Soorria Saruva <soorria.ss@gmail.com>
2022-12-01 21:43:20 +1100

    handle variant modifiers and fix peer versions

--- src/index.js
+++ src/index.js
@@ -1,15 +1,51 @@
 const plugin = require('tailwindcss/plugin')
 
-const hocusPlugin = plugin(({ addVariant }) => {
+/**
+ * @param {string} modifier
+ * @param {string} prefix
+ * @returns {string}
+ */
+const addModifierIfNeeded = (modifier, prefix) => {
+  return modifier ? `${prefix}\\/${modifier}` : prefix
+}
+
+const hocusPlugin = plugin(({ addVariant, matchVariant }) => {
   addVariant('hocus', ['&:hover', '&:focus'])
   addVariant('hocus-within', ['&:hover', '&:focus-within'])
   addVariant('hocus-visible', ['&:hover', '&:focus-visible'])
+
+  if (matchVariant) {
+    const variantValues = {
+      hocus: 'focus',
+      'hocus-within': 'focus-within',
+      'hocus-visible': 'focus-visible',
+    }
+
+    const variants = [
+      ['group', ' &'],
+      ['peer', ' ~ &'],
+    ]
+
+    for (const [name, selectorEnd] of variants) {
+      matchVariant(
+        name,
+        (value, { modifier }) => [
+          `:merge(.${addModifierIfNeeded(modifier, name)}):hover${selectorEnd}`,
+          `:merge(.${addModifierIfNeeded(modifier, name)}):${value}${selectorEnd}`,
+        ],
+        { values: variantValues }
+      )
+    }
+
+    return
+  }
+
   addVariant('group-hocus', [':merge(.group):hover &', ':merge(.group):focus &'])
   addVariant('group-hocus-within', [':merge(.group):hover &', ':merge(.group):focus-within &'])
   addVariant('group-hocus-visible', [':merge(.group):hover &', ':merge(.group):focus-visible &'])
-  addVariant('peer-hocus', [':merge(.peer):hover &', ':merge(.peer):focus &'])
-  addVariant('peer-hocus-within', [':merge(.peer):hover &', ':merge(.peer):focus-within &'])
-  addVariant('peer-hocus-visible', [':merge(.peer):hover &', ':merge(.peer):focus-visible &'])
+  addVariant('peer-hocus', [':merge(.peer):hover ~ &', ':merge(.peer):focus ~ &'])
+  addVariant('peer-hocus-within', [':merge(.peer):hover ~ &', ':merge(.peer):focus-within ~ &'])
+  addVariant('peer-hocus-visible', [':merge(.peer):hover ~ &', ':merge(.peer):focus-visible ~ &'])
 })
 
 module.exports = hocusPlugin
