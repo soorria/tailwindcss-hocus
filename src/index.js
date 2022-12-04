83768d4892b9f4734542c4b2b37bacf491c5fe50
Soorria Saruva <soorria.ss@gmail.com>
2022-12-04 16:10:04 +1100

    Handle the hoverOnlyWhenSupported option

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 const plugin = require('tailwindcss/plugin')
 
 /**
+ * Adapted from https://github.com/tailwindlabs/tailwindcss/blob/master/src/featureFlags.js
+ *
+ * @param {import('tailwindcss').Config} config
+ * @param {string} flag
+ * @returns {boolean}
+ */
+const futureFlagEnabled = (config, flag) => {
+  return config.future === 'all' || (config?.future?.[flag] ?? defaults[flag] ?? false)
+}
+
+/**
  * @param {string} modifier
  * @param {string} prefix
  * @returns {string}
@@ -9,43 +20,59 @@ const addModifierIfNeeded = (modifier, prefix) => {
   return modifier ? `${prefix}\\/${modifier}` : prefix
 }
 
-const hocusPlugin = plugin(({ addVariant, matchVariant }) => {
-  addVariant('hocus', ['&:hover', '&:focus'])
-  addVariant('hocus-within', ['&:hover', '&:focus-within'])
-  addVariant('hocus-visible', ['&:hover', '&:focus-visible'])
+const hocusPlugin = plugin(({ addVariant, matchVariant, config }) => {
+  const hoverOnlyWhenSupported = futureFlagEnabled(config(), 'hoverOnlyWhenSupported')
+  const wrapSelectorForHoverIfNeeded = (/** @type {string} */ selector) =>
+    hoverOnlyWhenSupported ? `@media (hover: hover) and (pointer: fine) { ${selector} }` : selector
+
+  const hoverSelector = wrapSelectorForHoverIfNeeded('&:hover')
+
+  addVariant('hocus', [hoverSelector, '&:focus'])
+  addVariant('hocus-within', [hoverSelector, '&:focus-within'])
+  addVariant('hocus-visible', [hoverSelector, '&:focus-visible'])
 
   if (matchVariant) {
-    const variantValues = {
+    const variantFocusSelectors = {
       hocus: 'focus',
       'hocus-within': 'focus-within',
       'hocus-visible': 'focus-visible',
     }
 
-    const variants = [
-      ['group', ' &'],
-      ['peer', ' ~ &'],
-    ]
+    const variantsEndParts = {
+      group: ' &',
+      peer: ' ~ &',
+    }
 
-    for (const [name, selectorEnd] of variants) {
+    for (const [name, selectorEnd] of Object.entries(variantsEndParts)) {
       matchVariant(
         name,
-        (value, { modifier }) => [
-          `:merge(.${addModifierIfNeeded(modifier, name)}):hover${selectorEnd}`,
-          `:merge(.${addModifierIfNeeded(modifier, name)}):${value}${selectorEnd}`,
-        ],
-        { values: variantValues }
+        (value, { modifier }) => {
+          const hoverSelector = wrapSelectorForHoverIfNeeded(
+            `:merge(.${addModifierIfNeeded(modifier, name)}):hover${selectorEnd}`
+          )
+          const focusSelector = `:merge(.${addModifierIfNeeded(
+            modifier,
+            name
+          )}):${value}${selectorEnd}`
+
+          return [hoverSelector, focusSelector]
+        },
+        { values: variantFocusSelectors }
       )
     }
 
     return
   }
 
-  addVariant('group-hocus', [':merge(.group):hover &', ':merge(.group):focus &'])
-  addVariant('group-hocus-within', [':merge(.group):hover &', ':merge(.group):focus-within &'])
-  addVariant('group-hocus-visible', [':merge(.group):hover &', ':merge(.group):focus-visible &'])
-  addVariant('peer-hocus', [':merge(.peer):hover ~ &', ':merge(.peer):focus ~ &'])
-  addVariant('peer-hocus-within', [':merge(.peer):hover ~ &', ':merge(.peer):focus-within ~ &'])
-  addVariant('peer-hocus-visible', [':merge(.peer):hover ~ &', ':merge(.peer):focus-visible ~ &'])
+  const groupHoverSelector = wrapSelectorForHoverIfNeeded(':merge(.group):hover &')
+  const peerHoverSelector = wrapSelectorForHoverIfNeeded(':merge(.peer):hover ~ &')
+
+  addVariant('group-hocus', [groupHoverSelector, ':merge(.group):focus &'])
+  addVariant('group-hocus-within', [groupHoverSelector, ':merge(.group):focus-within &'])
+  addVariant('group-hocus-visible', [groupHoverSelector, ':merge(.group):focus-visible &'])
+  addVariant('peer-hocus', [peerHoverSelector, ':merge(.peer):focus ~ &'])
+  addVariant('peer-hocus-within', [peerHoverSelector, ':merge(.peer):focus-within ~ &'])
+  addVariant('peer-hocus-visible', [peerHoverSelector, ':merge(.peer):focus-visible ~ &'])
 })
 
 module.exports = hocusPlugin
